9ce7eb71087f87ea3692fc554eb3789cf9519046
micmay1311 <mayankchouksey.ec@gmail.com>
2025-07-05 12:03:44 +0530

    Global controller advice added for Token invalid

--- src/main/java/com/capstone/EComProductService/controller/controllerAdvice/GlobalControllerAdvice.java
+++ src/main/java/com/capstone/EComProductService/controller/controllerAdvice/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.capstone.EComProductService.controller.controllerAdvice;
 
 import com.capstone.EComProductService.dto.ErrorResponseDTO;
+import com.capstone.EComProductService.exception.InvalidTokenException;
 import com.capstone.EComProductService.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,5 +14,13 @@ public class GlobalControllerAdvice {
         errorResponse.setMessage(ex.getMessage());
         errorResponse.setMessageCode(404);
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value= InvalidTokenException.class)
+    public ResponseEntity<ErrorResponseDTO> handleTokenInvalidException(Exception ex){
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+        errorResponseDTO.setMessage(ex.getMessage());
+        errorResponseDTO.setMessageCode(403);
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.FORBIDDEN);
     }
 }
